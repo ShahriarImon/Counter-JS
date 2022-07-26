fbfc37be706f42b28faee1e98a05e00fc0d4cfe2
ShahriarImon <imon.shahriar012@gmail.com>
2022-07-26 14:14:37 +0600

    counter implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,34 @@
-const btn = document.getElementById("button");
-const colors = ["red", "grey", "tomato", "lightgreen", "blue"];
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const reset = document.getElementById("reset");
+const counter = document.getElementById("counter");
 
-btn.addEventListener("click", (e) => {
-  const random = Math.floor(Math.random() * colors.length);
-  document.body.style.backgroundColor = colors[random];
-  document.getElementById(
-    "color"
-  ).textContent = `Background Color: ${colors[random]}`;
-});
+let count = 0;
+
+const clicking = (element) => {
+  element.addEventListener("click", (e) => {
+    if (e.currentTarget.id == "add") {
+      count += 1;
+    }
+    if (e.target.id == "minus") {
+      count -= 1;
+    }
+    if (e.target.id == "reset") {
+      count = 0;
+    }
+
+    if (count > 0) {
+      counter.style.color = "green";
+    } else {
+      counter.style.color = "red";
+    }
+    counter.textContent = `Counter : ${count}`;
+  });
+};
+
+clicking(add);
+console.log("1");
+clicking(minus);
+console.log("2");
+clicking(reset);
+console.log("3");
